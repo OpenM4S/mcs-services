6ceaf27db520ca7b4fbfac14a0a421a84e499322
Saleem Ullah <saleem.ullah.88@gmail.com>
2014-05-15 12:28:02 +0500

    + modified model fields

--- mcs/appreq/models.py
+++ mcs/appreq/models.py
@@ -2,43 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext as _
 # Create your models here.
 
-class Request(models.Model):
-    request_no = models.CharField(_("request_no"), max_length=10, null=False, blank=False, unique=True)
-    first_name = models.CharField(_("request_first_name"), max_length=30, blank=False, null=False)
-    middle_name = models.CharField(_("request_middle_name"), max_length=30, null=True)
-    last_name = models.CharField(_("request_last_name"), max_length=30, null=False)
-    cnic = models.CharField(_("request_cnic"), null=False, blank=False, max_length=15)
-    email = models.EmailField(_("request_email"), null=True, max_length=254)
-    phone = models.CharField(_("request_phone"), null=True, max_length=20)
-    request_date = models.DateTimeField(_("request_date"), null=False, auto_now=True)
-    # FK
-    license_type = models.IntegerField(_("license_type_id"), null=False)
-    # FK
-    mineral_type = models.IntegerField(_("mineral_type_id"), null=False)
-    # FK
-    total_area = models.IntegerField(_("total_area"), null=False)
-    # FK
-    unit_type = models.IntegerField(_("unit_type_id"), null=False)
-    # FK
-    topo_sheet = models.IntegerField(_("topo_sheet_no"), max_length=100, null=False)
-    # FK
-    request_status = models.IntegerField(_("request_status"), null=False)
-    request_status_date = models.DateTimeField(_("request_status_date"), null=False)
-    request_status_remarks = models.CharField(_("request_status_remarks"), max_length=900, null=True)
-
-    class Meta:
-        verbose_name = "Application Request"
-
-class Coordinate(models.Model):
-    request = models.ForeignKey(Request, related_name='coordinates')
-    request_coord_no = models.CharField(_("request_coordinates_no"), max_length=20, null=False)
-    latitude = models.DecimalField(_("latitude"), max_digits=8, decimal_places=5, null=False)
-    longitude = models.DecimalField(_("longitude"), max_digits=8, decimal_places=5, null=False)
-    elevation = models.DecimalField(_("elevation"), max_digits=15, decimal_places=5, null=True)
-
-    class Meta:
-        verbose_name = "coordinates"
-
 class CodeMaster(models.Model):
     # cm_id
     name  = models.CharField(_("name"), max_length=30, null=False)
@@ -95,3 +58,38 @@ class License(models.Model):
     expire_before = models.IntegerField(_("expire_before"), null=False)
     istransferable = models.BooleanField()
     isextendable = models.BooleanField()
+
+class Request(models.Model):
+    request_no = models.CharField(_("request_no"), max_length=10, null=False, blank=False, unique=True)
+    first_name = models.CharField(_("request_first_name"), max_length=30, blank=False, null=False)
+    middle_name = models.CharField(_("request_middle_name"), max_length=30, null=True)
+    last_name = models.CharField(_("request_last_name"), max_length=30, null=False)
+    cnic = models.CharField(_("request_cnic"), null=False, blank=False, max_length=15)
+    email = models.EmailField(_("request_email"), null=True, max_length=254)
+    phone = models.CharField(_("request_phone"), null=True, max_length=20)
+    request_date = models.DateTimeField(_("request_date"), null=False, auto_now=True)
+    # FK
+    license_type = models.ForeignKey(CodeDetail, related_name='request_license_type')
+    # FK
+    mineral_type = models.ForeignKey(CodeDetail, related_name='request_mineral_type')
+    # FK
+    total_area = models.IntegerField(_("total_area"), null=False)
+    # FK
+    topo_sheet = models.IntegerField(_("topo_sheet"), null=False)
+    # FK
+    request_status = models.IntegerField(_("request_status"), null=False)
+    request_status_date = models.DateTimeField(_("request_status_date"), null=False)
+    request_status_remarks = models.CharField(_("request_status_remarks"), max_length=900, null=True)
+
+    class Meta:
+        verbose_name = "Application Request"
+
+class Coordinate(models.Model):
+    request = models.ForeignKey(Request, related_name='coordinates')
+    request_coord_no = models.CharField(_("request_coordinates_no"), max_length=20, null=False)
+    latitude = models.DecimalField(_("latitude"), max_digits=8, decimal_places=5, null=False)
+    longitude = models.DecimalField(_("longitude"), max_digits=8, decimal_places=5, null=False)
+    elevation = models.DecimalField(_("elevation"), max_digits=15, decimal_places=5, null=True)
+
+    class Meta:
+        verbose_name = "coordinates"
